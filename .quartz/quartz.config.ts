b3c8000b5bc9f491c9378bd36a180b2cacd4113f
k3v <25158905+k3v53@users.noreply.github.com>
2024-09-05 05:35:40 -0300

    Added Plugin.HardLineBreaks() to quartz Config

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -52,6 +52,7 @@ const config: QuartzConfig = {
 	},
 	plugins: {
 		transformers: [
+			Plugin.HardLineBreaks(),
 			Plugin.FrontMatter(),
 			Plugin.CreatedModifiedDate({
 				priority: ['frontmatter', 'filesystem'],
